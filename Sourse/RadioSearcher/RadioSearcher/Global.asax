8cb366aee8bcd2b9de28c45c9d9f57bce08bf9e7
Карачун Антон <antoha54785485@mail.ru>
2017-11-14 21:53:12 +0300

    Init MVC.

---
+++ Sourse/RadioSearcher/RadioSearcher/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="RadioSearcher.MvcApplication" Language="C#" %>
---
+++ Sourse/RadioSearcher/RadioSearcher/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(RadioSearcher.Startup))]
+namespace RadioSearcher
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
